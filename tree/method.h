bc5084aa58e1f2f66609c6fddca537ca93f56104
KaioGabriel-the <kkaiogabrielk@gmail.com>
2025-05-29 21:43:29 -0300

    ✨ feat/ buscas: algoritmos de buscas em árvores.

--- tree/method.h
+++ tree/method.h
@@ -4,6 +4,30 @@
 #include <stdio.h>
 #include "tree.h"
 
+void inorder(BRANCH A){
+	if (A == NULL) return;
+
+	inorder(A->left);
+	printf("%d ", A->number);
+	inorder(A->right);
+}
+
+void preorder(BRANCH A){
+	if (A == NULL) return;
+
+	printf("%d ", A->number);
+	preorder(A->left);
+	preorder(A->right);
+}
+
+void postorder(BRANCH A){
+	if (A == NULL) return;
+
+	postorder(A->left);
+	postorder(A->right);
+	printf("%d ", A->number);
+}
+
 void run(BRANCH A){
 	if (A == NULL) return;
 
